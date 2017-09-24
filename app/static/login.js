06886ebe4a8a597a8923b53922bc44a24bfe8ad6
Austin Meyer <frizzkitten@gmail.com>
2017-09-24 11:57:59 -0400

    small tweaks

--- app/static/login.js
+++ app/static/login.js
@@ -39,38 +39,12 @@ $(document).ready(function() {
           event.preventDefault();
         });
 
-        // var RESPONSE = false;
-        // if (RESPONSE == true) {
-        //     //login success, do animation
-        //     // $('form').fadeOut(500);
-        //     // $('.wrapper').addClass('form-success');
-        //     //
-        //     // //wait for animation then go to account page
-        //     // setTimeout(function(){
-        //     //     $(".container").fadeOut(1000);
-        //     //     location.href = "account.html";
-        //     // }, 1000);
-        // } else {
-        //     //login failed
-        //     // var NO_MATCHING_USER = true;
-        //     // if (NO_MATCHING_USER) {
-        //     //     $("#error").css('visibility', 'visible')
-        //     //     $("#error").attr("value","Username incorrect");
-        //     // } else if (WRONG_PASSWORD) {
-        //     //     $("#error").css('visibility', 'visible')
-        //     //     $("#error").attr("value","Password incorrect");
-        //     // } else {
-        //     //     $("#error").css('visibility', 'visible')
-        //     //     $("#error").attr("value","Login failed");
-        //     // }
-        // }
-    //});
-
     $("#sign-up-button").click(function(event){
         console.log("submitting sign up form")
         event.preventDefault();
 
         $('form').fadeOut(500);
+        $('#info').fadeOut(500);
         $('.wrapper').addClass('form-success');
 
         //AJAX CALL TO CREATE ACCOUNT, response is RESPONSE
